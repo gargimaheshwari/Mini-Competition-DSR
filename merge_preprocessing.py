ae05a1023fbae2e8bf7a7658806b0c82a395af45
Gargi Maheshwari <gargimaheshwari@users.noreply.github.com>
2019-07-15 13:53:53 +0200

    Removed labels not present in test data

--- merge_preprocessing.py
+++ merge_preprocessing.py
@@ -11,38 +11,21 @@ data["Date"] = pd.to_datetime(data["Date"])
 data["Month"] = data["Date"].dt.month
 data["Day"] = data["Date"].dt.day
 data["Year"] = data["Date"].dt.year
+data["WeekOfYear"] = data["Date"].dt.weekofyear
 
 data.loc[:, "StateHoliday"] = data.loc[:, "StateHoliday"].map({0:0,'0':0, 'a':1,'b':2,'c':3})
-data.loc[:, "StoreType"] = data.loc[:, "StoreType"].map({'a':1,'b':2,'c':3, 'd':4})
-data.loc[:, "Assortment"] = data.loc[:, "Assortment"].map({'a':1,'b':2,'c':3})
+#data.loc[:, "StoreType"] = data.loc[:, "StoreType"].map({'a':1,'b':2,'c':3, 'd':4})
+#data.loc[:, "Assortment"] = data.loc[:, "Assortment"].map({'a':1,'b':2,'c':3})
 #data.loc[:, "PromoInterval"] = data.loc[:,"PromoInterval"].map({"Jan,Apr,Jul,Oct":1, "Feb,May,Aug,Nov":2, "Mar,Jun,Sept,Dec":3})
 
-data.loc[:, "CompetitionDistance"].fillna(data.loc[:, "CompetitionDistance"].mean(), inplace = True)
+#data.loc[:, "CompetitionDistance"].fillna(data.loc[:, "CompetitionDistance"].mean(), inplace = True)
 
 data.fillna(0, inplace=True)
-data["WeekOfYear"] = data["Date"].dt.weekofyear
-data['CompetitionOpen'] = 12 * (data.Year - data.CompetitionOpenSinceYear) + (data.Month - data.CompetitionOpenSinceMonth)
-data['PromoOpen'] = 12 * (data.Year - data.Promo2SinceYear) + (data.WeekOfYear - data.Promo2SinceWeek) / 4.0
-data['PromoOpen'] = data.PromoOpen.apply(lambda x: x if x > 0 else 0)
-data.loc[data.Promo2SinceYear == 0, 'PromoOpen'] = 0
-month2str = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', \
-             7:'Jul', 8:'Aug', 9:'Sept', 10:'Oct', 11:'Nov', 12:'Dec'}
-data['monthStr'] = data.Month.map(month2str)
-data.loc[data.PromoInterval == 0, 'PromoInterval'] = ''
-data['IsPromoMonth'] = 0
-for interval in data.PromoInterval.unique():
-    if interval != '':
-        for month in interval.split(','):
-            data.loc[(data.monthStr == month) & (data.PromoInterval == interval), 'IsPromoMonth'] = 1
 
-
-features = ["Store", "DayOfWeek", "Sales",
-            "Open", "Promo",
-            "StateHoliday", "SchoolHoliday",
-            "StoreType", "Assortment",
-            "CompetitionDistance", "Promo2", 
+features = ["Store", "DayOfWeek", "Sales", "Promo",
+            "StateHoliday", "SchoolHoliday", 
             "Month", "Day", "Customers",
-            "WeekOfYear", "CompetitionOpen", "PromoOpen", "IsPromoMonth"]
+            "WeekOfYear", "Year"]
 
 data = data.loc[:, features]
 
