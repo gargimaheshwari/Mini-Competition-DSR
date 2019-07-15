f993506314ad5830aa471d7cb0f3170f2523d70f
Gargi Maheshwari <gargimaheshwari@users.noreply.github.com>
2019-07-15 13:53:00 +0200

    Changed xgbooster, included pre-processing

--- output.py
+++ output.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 from joblib import load
+import xgboost
 
 def metric(preds, actuals):
     preds = preds.reshape(-1)
@@ -10,35 +10,54 @@ def metric(preds, actuals):
     assert preds.shape == actuals.shape
     return 100 * np.linalg.norm((actuals - preds) / actuals) / np.sqrt(preds.shape[0])
 
-def plot_feature_importances(rf, cols, model_dir):
-    importances = pd.DataFrame()
-    importances.loc[:, 'importances'] = rf.feature_importances_
-    importances.loc[:, 'features'] = cols
-    importances.sort_values('importances', inplace=True)
+def plot_feature_importances(rf, cols):
+    importances = rf.get_fscore()
+    importances = importances.items()
+    df = pd.DataFrame(importances, columns = ['feature', 'fscore'])
+    df['fscore'] = df['fscore'] / df['fscore'].sum()
+    df.sort_values('fscore', inplace=True)
+    
     
     f, a = plt.subplots()
-    importances.plot(ax=a, kind='bar', x='features', y='importances')
+    df.plot(ax = a, kind='bar', x='feature', y='fscore')
     plt.gcf().subplots_adjust(bottom=0.3)
-    f.savefig(os.path.join(model_dir, 'importances.png'))
+    f.savefig('importances.png')
 
 def predictor(test_features, rf):
-    return rf.predict(test_features)
+    return rf.predict(xgboost.DMatrix(test_features))
 
-test_data = pd.read_csv("rossmann-store-sales/test_data.csv", index_col = 0)
+#load data and model
+test_data = pd.read_csv("rossmann-store-sales/new_test.csv", index_col = 0)
 rf = load("xgb.joblib")
 
+#Pre-processing
+test_data = test_data[test_data["Open"] == 1]
+test_data = test_data[test_data["Sales"] > 0]
+
+test_data["Date"] = pd.to_datetime(test_data["Date"])
+test_data["Month"] = test_data["Date"].dt.month
+test_data["Day"] = test_data["Date"].dt.day
+test_data["Year"] = test_data["Date"].dt.year
+
+test_data.loc[:, "StateHoliday"] = test_data.loc[:, "StateHoliday"].map({0:0,'0':0, 'a':1,'b':2,'c':3})
+#test_data.loc[:, "StoreType"] = test_data.loc[:, "StoreType"].map({'a':1,'b':2,'c':3, 'd':4})
+#test_data.loc[:, "Assortment"] = test_data.loc[:, "Assortment"].map({'a':1,'b':2,'c':3})
+#test_data.loc[:, "PromoInterval"] = test_data.loc[:,"PromoInterval"].map({"Jan,Apr,Jul,Oct":1, "Feb,May,Aug,Nov":2, "Mar,Jun,Sept,Dec":3})
+
+#test_data.loc[:, "CompetitionDistance"].fillna(test_data.loc[:, "CompetitionDistance"].mean(), inplace = True)
+
+features = ["Store", "DayOfWeek", "Sales", "Promo",
+            "StateHoliday", "SchoolHoliday", 
+            "Month", "Day", "Customers",
+            "WeekOfYear", "Year"]
+
+test_data = test_data.loc[:, features]
+
+#Split columns
 test_labels = np.array(test_data.loc[:, "Sales"])
 test_features = test_data.drop("Sales", axis = 1)
-model_dir = "C:\\Users\\Gargi\\Documents\\DataScience\\DSR\\Mini-competition\\"
 features = test_features.columns
 
 preds = predictor(test_features, rf)
 print('RMSPE:', metric(preds, test_labels))
-plot_feature_importances(rf, features, model_dir)
-
-rf = load("xgb.joblib")
-train_data = pd.read_csv("rossmann-store-sales/train_data.csv", index_col = 0)
-train_labels = train_data.loc[:, "Sales"]
-train_features = train_data.drop("Sales", axis = 1)
-preds = predictor(train_features, rf)
-print('RMSPE:', metric(preds, np.array(train_labels)))
+plot_feature_importances(rf, features)
